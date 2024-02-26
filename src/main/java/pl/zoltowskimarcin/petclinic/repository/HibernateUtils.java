74964118c88aac2dec6069bf0db11a97c96c3c1b
marcin <m.s.zoltowski@gmail.com>
2024-02-26 22:16:38 +0100

    Add singleton and hibernate config file

---
+++ src/main/java/pl/zoltowskimarcin/petclinic/repository/HibernateUtils.java
@@ -0,0 +1,34 @@
+package pl.zoltowskimarcin.petclinic.repository;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+public class HibernateUtils {
+
+    private static SessionFactory sessionFactory;
+
+    static {
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+        try {
+            sessionFactory = new MetadataSources(serviceRegistry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+        } catch (Exception e) {
+            e.printStackTrace();
+            StandardServiceRegistryBuilder.destroy(serviceRegistry);
+        }
+    }
+
+    private HibernateUtils() {
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+
+}
