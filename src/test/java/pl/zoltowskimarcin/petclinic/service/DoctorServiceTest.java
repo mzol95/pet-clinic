035b2693b7220d391f67d4a7cce0d6808c6205b5
marcin <m.s.zoltowski@gmail.com>
2024-03-02 17:27:14 +0100

    Add tests

--- src/test/java/pl/zoltowskimarcin/petclinic/service/DoctorServiceTest.java
+++ src/test/java/pl/zoltowskimarcin/petclinic/service/DoctorServiceTest.java
@@ -2,26 +2,31 @@ package pl.zoltowskimarcin.petclinic.service;
 
 import liquibase.exception.CommandExecutionException;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import pl.zoltowskimarcin.petclinic.repository.jpa.DoctorRepository;
+import pl.zoltowskimarcin.petclinic.exception.client.EntityDeletingFailedException;
+import pl.zoltowskimarcin.petclinic.exception.client.EntityException;
+import pl.zoltowskimarcin.petclinic.exception.client.EntityReadingFailedException;
+import pl.zoltowskimarcin.petclinic.exception.client.EntityUpdatingFailedException;
 import pl.zoltowskimarcin.petclinic.utils.DatabaseInitializer;
 import pl.zoltowskimarcin.petclinic.web.model.DoctorDto;
+
+import java.util.Optional;
 
 @SpringBootTest
 class DoctorServiceTest {
 
-    public static final String DOCTOR_TEST_NAME = "Test name";
-    public static final String DOCTOR_TEST_SURNAME = "Test surname";
-
-    public static final String DOCTOR_UPDATE_SURNAME = "Update surname";
-    public static final String DOCTOR_UPDATE_NAME = "Update name";
+    private static final String DOCTOR_TEST_NAME = "Test name";
+    private static final String DOCTOR_TEST_SURNAME = "Test surname";
+    private static final String DOCTOR_UPDATE_SURNAME = "Update surname";
+    private static final String DOCTOR_UPDATE_NAME = "Update name";
+    private static final Long ID_1 = 1L;
 
     @Autowired
     private DoctorService doctorService;
-    private DoctorRepository doctorRepository;
 
     private DoctorDto doctorDto;
     private DoctorDto updatedDoctorDto;
@@ -48,24 +53,84 @@ class DoctorServiceTest {
     }
 
     @Test
-    void saveDoctor() {
+    void creating_new_doctor_should_return_created_doctor() throws EntityException {
+        //given
+
+        //when
+        DoctorDto returnedDoctor = doctorService.saveDoctor(doctorDto);
+
+        //then
+        Assertions.assertEquals(doctorDto, returnedDoctor, "Doctor is not equal");
+    }
+
+    @Test
+    void read_after_creating_new_doctor_should_return_newly_created_doctor() throws EntityException {
+        //given
+
+        //when
+        DoctorDto persistedDoctor = doctorService.saveDoctor(doctorDto);
+        DoctorDto returnedDoctor = doctorService.getDoctorById(ID_1)
+                .orElseThrow(EntityReadingFailedException::new);
+
+        //then
+        Assertions.assertEquals(doctorDto, returnedDoctor, "Doctor is not equal");
+    }
+
+    @Test
+    void after_updating_should_return_updated_doctor_entity() throws EntityException {
+        //given
+
+        //when
+        DoctorDto persistedDoctor = doctorService.saveDoctor(doctorDto);
+        DoctorDto updatedDoctor = doctorService.updateDoctor(ID_1, updatedDoctorDto);
+
+        //then
+        Assertions.assertEquals(updatedDoctorDto, updatedDoctor, "Doctor is not equal");
+    }
+
+    @Test
+    void updating_not_existing_entity_should_throw_doctor_updating_failed_exception() {
         //given
 
         //when
 
         //then
-
+        Assertions.assertThrows(EntityUpdatingFailedException.class,
+                () -> doctorService.updateDoctor(ID_1, updatedDoctorDto), "Exception not thrown");
     }
 
     @Test
-    void getDoctor() {
+    void after_deleting_doctor_should_return_null_when_try_to_read_deleted_entity() throws EntityException {
+        //given
+
+        //when
+        DoctorDto persistedDoctor = doctorService.saveDoctor(doctorDto);
+        doctorService.deleteDoctor(ID_1);
+        DoctorDto returnedDoctor = doctorService.getDoctorById(ID_1).orElse(null);
+
+        //then
+        Assertions.assertNull(returnedDoctor, "Doctor is not null");
     }
 
     @Test
-    void updateDoctor() {
+    void no_entity_found_while_reading_should_return_empty_optional() throws EntityReadingFailedException {
+        //given
+
+        //when
+        Optional<DoctorDto> returnedDoctor = doctorService.getDoctorById(ID_1);
+
+        //then
+        Assertions.assertEquals(Optional.empty(), returnedDoctor, "Doctor is not empty");
     }
 
     @Test
-    void deleteDoctor() {
+    void deleting_not_existing_entity_should_throw_doctor_deleting_failed_exception() {
+        //given
+
+        //when
+
+        //then
+        Assertions.assertThrows(EntityDeletingFailedException.class,
+                () -> doctorService.deleteDoctor(ID_1), "Exception not thrown");
     }
 }
