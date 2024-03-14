ed112dfeae78e2afe0383dcd54e5ee5d75a91d68
marcin <m.s.zoltowski@gmail.com>
2024-03-14 22:18:38 +0100

    Add tests

--- src/test/java/pl/zoltowskimarcin/petclinic/mapper/ClientMapperTest.java
+++ src/test/java/pl/zoltowskimarcin/petclinic/mapper/ClientMapperTest.java
@@ -2,8 +2,11 @@ package pl.zoltowskimarcin.petclinic.mapper;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import pl.zoltowskimarcin.petclinic.repository.entity.Address;
 import pl.zoltowskimarcin.petclinic.repository.entity.Client;
 import pl.zoltowskimarcin.petclinic.web.model.cilent.ClientDto;
+
+import java.util.List;
 
 import static pl.zoltowskimarcin.petclinic.utils.TestUtils.*;
 
@@ -12,37 +15,135 @@ class ClientMapperTest {
     @Test
     void map_from_model_to_entity() {
         //given
-        ClientDto clientDto = new ClientDto(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, null, null, null);
+        Address address = new Address(CLIENT_JON_ADDRESS_STREET, CLIENT_JON_ADDRESS_CITY, CLIENT_JON_ADDRESS_POSTAL_CODE);
+        ClientDto clientDto = new ClientDto(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, address, null, null);
 
         //when
         Client mappedClientEntity = new ClientMapper().mapToEntity(clientDto);
         String mappedName = mappedClientEntity.getName();
         String mappedSurname = mappedClientEntity.getSurname();
         String mappedPhone = mappedClientEntity.getPhone();
+        Address mappedAddress = mappedClientEntity.getAddress();
 
         //then
         Assertions.assertAll(
                 () -> Assertions.assertEquals(CLIENT_NAME_JON, mappedName, "Name is not mapped correctly"),
                 () -> Assertions.assertEquals(CLIENT_SURNAME_SNOW, mappedSurname, "Surname is not mapped correctly"),
-                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone, "Phone is not mapped correctly")
+                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress, "Address is not mapped correctly")
         );
     }
 
     @Test
     void map_from_entity_to_model() {
         //given
-        Client client = new Client(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, null, null, null);
+        Address address = new Address(CLIENT_JON_ADDRESS_STREET, CLIENT_JON_ADDRESS_CITY, CLIENT_JON_ADDRESS_POSTAL_CODE);
+        Client client = new Client(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, address, null, null);
 
         //when
         ClientDto mappedClient = new ClientMapper().mapToDto(client, ClientDto.class);
         String mappedName = mappedClient.getName();
         String mappedSurname = mappedClient.getSurname();
         String mappedPhone = mappedClient.getPhone();
+        Address mappedAddress = mappedClient.getAddress();
         //then
         Assertions.assertAll(
                 () -> Assertions.assertEquals(CLIENT_NAME_JON, mappedName, "Name is not mapped correctly"),
                 () -> Assertions.assertEquals(CLIENT_SURNAME_SNOW, mappedSurname, "Surname is not mapped correctly"),
-                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone, "Phone is not mapped correctly")
+                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress, "Address is not mapped correctly")
+        );
+    }
+
+    @Test
+    void map_list_from_model_to_entity() {
+        //given
+        Address address = new Address(CLIENT_JON_ADDRESS_STREET, CLIENT_JON_ADDRESS_CITY, CLIENT_JON_ADDRESS_POSTAL_CODE);
+        ClientDto clientDto1 = new ClientDto(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, address, null, null);
+        ClientDto clientDto2 = new ClientDto(CLIENT_NAME_KHAL, CLIENT_SURNAME_DROGO, CLIENT_KHAL_PHONE_111_222_333, address, null, null);
+        ClientDto clientDto3 = new ClientDto(CLIENT_NAME_NED, CLIENT_SURNAME_STARK, CLIENT_NED_PHONE_123_000_987, address, null, null);
+
+        List<ClientDto> clientDtos = List.of(clientDto1, clientDto2, clientDto3);
+
+        //when
+        List<Client> mappedClients = new ClientMapper().mapToEntityList(clientDtos);
+
+        String mappedName1 = mappedClients.get(0).getName();
+        String mappedSurname1 = mappedClients.get(0).getSurname();
+        String mappedPhone1 = mappedClients.get(0).getPhone();
+        Address mappedAddress1 = mappedClients.get(0).getAddress();
+
+        String mappedName2 = mappedClients.get(1).getName();
+        String mappedSurname2 = mappedClients.get(1).getSurname();
+        String mappedPhone2 = mappedClients.get(1).getPhone();
+        Address mappedAddress2 = mappedClients.get(1).getAddress();
+
+        String mappedName3 = mappedClients.get(2).getName();
+        String mappedSurname3 = mappedClients.get(2).getSurname();
+        String mappedPhone3 = mappedClients.get(2).getPhone();
+        Address mappedAddress3 = mappedClients.get(2).getAddress();
+
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(3, mappedClients.size(), "List size isn't equal 3"),
+                () -> Assertions.assertEquals(CLIENT_NAME_JON, mappedName1, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_SNOW, mappedSurname1, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone1, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress1, "Address is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NAME_KHAL, mappedName2, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_DROGO, mappedSurname2, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_KHAL_PHONE_111_222_333, mappedPhone2, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress2, "Address is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NAME_NED, mappedName3, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_STARK, mappedSurname3, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NED_PHONE_123_000_987, mappedPhone3, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress3, "Address is not mapped correctly")
+        );
+    }
+
+    @Test
+    void map_list_from_entity_to_model() {
+        //given
+        Address address = new Address(CLIENT_JON_ADDRESS_STREET, CLIENT_JON_ADDRESS_CITY, CLIENT_JON_ADDRESS_POSTAL_CODE);
+        Client clientDto1 = new Client(CLIENT_NAME_JON, CLIENT_SURNAME_SNOW, CLIENT_JON_PHONE_123_456_789, address, null, null);
+        Client clientDto2 = new Client(CLIENT_NAME_KHAL, CLIENT_SURNAME_DROGO, CLIENT_KHAL_PHONE_111_222_333, address, null, null);
+        Client clientDto3 = new Client(CLIENT_NAME_NED, CLIENT_SURNAME_STARK, CLIENT_NED_PHONE_123_000_987, address, null, null);
+
+        List<Client> clients = List.of(clientDto1, clientDto2, clientDto3);
+
+        //when
+        List<ClientDto> mappedClients = new ClientMapper().mapToDtoList(clients, ClientDto.class);
+
+        String mappedName1 = mappedClients.get(0).getName();
+        String mappedSurname1 = mappedClients.get(0).getSurname();
+        String mappedPhone1 = mappedClients.get(0).getPhone();
+        Address mappedAddress1 = mappedClients.get(0).getAddress();
+
+        String mappedName2 = mappedClients.get(1).getName();
+        String mappedSurname2 = mappedClients.get(1).getSurname();
+        String mappedPhone2 = mappedClients.get(1).getPhone();
+        Address mappedAddress2 = mappedClients.get(1).getAddress();
+
+        String mappedName3 = mappedClients.get(2).getName();
+        String mappedSurname3 = mappedClients.get(2).getSurname();
+        String mappedPhone3 = mappedClients.get(2).getPhone();
+        Address mappedAddress3 = mappedClients.get(2).getAddress();
+
+        //then
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(3, mappedClients.size(), "List size isn't equal 3"),
+                () -> Assertions.assertEquals(CLIENT_NAME_JON, mappedName1, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_SNOW, mappedSurname1, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_JON_PHONE_123_456_789, mappedPhone1, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress1, "Address is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NAME_KHAL, mappedName2, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_DROGO, mappedSurname2, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_KHAL_PHONE_111_222_333, mappedPhone2, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress2, "Address is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NAME_NED, mappedName3, "Name is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_SURNAME_STARK, mappedSurname3, "Surname is not mapped correctly"),
+                () -> Assertions.assertEquals(CLIENT_NED_PHONE_123_000_987, mappedPhone3, "Phone is not mapped correctly"),
+                () -> Assertions.assertEquals(address, mappedAddress3, "Address is not mapped correctly")
         );
     }
 }
--- src/test/java/pl/zoltowskimarcin/petclinic/utils/TestUtils.java
+++ src/test/java/pl/zoltowskimarcin/petclinic/utils/TestUtils.java
@@ -9,18 +9,28 @@ public class TestUtils {
     public static final String CLIENT_NAME_JON = "Jon";
     public static final String CLIENT_SURNAME_SNOW = "Snow";
     public static final String CLIENT_JON_PHONE_123_456_789 = "123-456-789";
+    public static final String CLIENT_JON_ADDRESS_STREET = "street1";
+    public static final String CLIENT_JON_ADDRESS_CITY = "city1";
+    public static final String CLIENT_JON_ADDRESS_POSTAL_CODE = "postal code1";
 
     public static final String CLIENT_NAME_NED = "Ned";
     public static final String CLIENT_SURNAME_STARK = "Stark";
     public static final String CLIENT_NED_PHONE_123_000_987 = "123-000-987";
+    public static final String CLIENT_NED_ADDRESS_STREET = "street2";
+    public static final String CLIENT_NED_ADDRESS_CITY = "city2";
+    public static final String CLIENT_NED_ADDRESS_POSTAL_CODE = "postal code2";
 
     public static final String CLIENT_NAME_KHAL = "Khal";
     public static final String CLIENT_SURNAME_DROGO = "Drogo";
     public static final String CLIENT_KHAL_PHONE_111_222_333 = "111-222-333";
+    public static final String CLIENT_KHAL_ADDRESS_STREET = "street3";
+    public static final String CLIENT_KHAL_ADDRESS_CITY = "city3";
+    public static final String CLIENT_KHAL_ADDRESS_POSTAL_CODE = "postal code3";
 
     public static final String UPDATE_CLIENT_NAME_TYRION = "Tyrion";
     public static final String UPDATE_CLIENT_SURNAME_LANNISTER = "Lannister";
     public static final String UPDATE_CLIENT_PHONE_987_654_321 = "987-654-321";
+
 
     public static final int LIST_SIZE_2 = 2;
     public static final Long ID_1 = 1L;
@@ -30,7 +40,6 @@ public class TestUtils {
 
     public static final String UPDATE_DOCTOR_NAME_ALLISON = "Allison";
     public static final String UPDATE_DOCTOR_SURNAME_CAMERON = "Cameron";
-
 
     public static final String PET_NAME_GARFIELD = "Garfield";
     public static final Gender PET_GENDER_MALE = Gender.MALE;
@@ -42,7 +51,6 @@ public class TestUtils {
 
     public static final LocalDateTime APPOINTMENT_DATE_TIME = LocalDateTime.of(1900, 02, 02, 13, 11);
     public static final boolean APPOINTMENT_FINISHED = true;
-
 
     public static final LocalDateTime UPDATE_APPOINTMENT_DATE_TIME = LocalDateTime.of(2000, 01, 01, 12, 00);
     public static final boolean UPDATE_APPOINTMENT_FINISHED = false;
